d441066b1b42861c782a596dad849fced3f8c745
Álvaro Arias Rivero <aarias@quodem.com>
2022-12-14 15:23:52 +0100

    update script to manipulate header

--- version2/popup.js
+++ version2/popup.js
@@ -3,9 +3,11 @@ $( document ).ready(function() {
         $('#disabled').hide();
     };
     $('#popup a.absolute-link').addClass('dynamic-class');
+    $('body header').css('z-index', '8');
 });
 
 $('#exit').on('click', function(){
     $('#disabled').hide();
     document.cookie = `accept_terms=true; expires=${new Date(new Date().getTime()+1000*60*60*24*365).toGMTString()}; path=/`;
+    $('body header').css('z-index', '99');
 });
--- version3/popup.js
+++ version3/popup.js
@@ -2,9 +2,11 @@ $( document ).ready(function() {
     if(document.cookie.indexOf("accept_terms" + "=") >= 0){
         $('#disabled').hide();
     };
+    $('body header').css('z-index', '8');
 });
 
 $('#exit').on('click', function(){
     $('#disabled').hide();
     document.cookie = `accept_terms=true; expires=${new Date(new Date().getTime()+1000*60*60*24*365).toGMTString()}; path=/`;
+    $('body header').css('z-index', '99');
 });
